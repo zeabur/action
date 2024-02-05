8bc6ea492542369ee0f161f78fe0380feb61874b
Yi-Jyun Pan <yjpan@zeabur.com>
2024-02-05 17:34:03 +0800

    feat(proc/golang): Support optional (don't break) flag

--- procedures/golang/download.go
+++ procedures/golang/download.go
@@ -7,17 +7,32 @@ import (
 	"os/exec"
 
 	zbaction "github.com/zeabur/action"
+	"golang.org/x/exp/slog"
 )
 
 func init() {
 	zbaction.RegisterProcedure("action/golang/mod-download", func(args zbaction.ProcStepArgs) (zbaction.ProcedureStep, error) {
-		return &DownloadAction{}, nil
+		optional := args["optional"]
+		if optional == "" {
+			optional = "false"
+		}
+
+		return &DownloadAction{
+			Optional: zbaction.NewArgumentBool(optional),
+		}, nil
 	})
 }
 
-type DownloadAction struct{}
+type DownloadAction struct {
+	// Optional is a flag to download optional dependencies.
+	//
+	// Default: false
+	Optional zbaction.Argument[bool]
+}
 
 func (d DownloadAction) Run(ctx context.Context, sc *zbaction.StepContext) (zbaction.CleanupFn, error) {
+	optional := d.Optional.Value(sc.ExpandString)
+
 	// Download dependencies. FIXME: cachable
 	cmd := exec.CommandContext(ctx, "go", "mod", "download")
 	cmd.Dir = sc.Root()
@@ -25,6 +40,11 @@ func (d DownloadAction) Run(ctx context.Context, sc *zbaction.StepContext) (zbac
 	cmd.Stderr = os.Stderr
 	cmd.Env = os.Environ()
 	if err := cmd.Run(); err != nil {
+		if optional {
+			slog.Error("failed to download mod (optional)", slog.String("error", err.Error()))
+			return nil, nil
+		}
+
 		return nil, fmt.Errorf("download mod: %w", err)
 	}
 
